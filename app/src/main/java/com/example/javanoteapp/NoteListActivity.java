2b8b536b1da86996165c84d9ccfef90cfa87d777
Emmanuel Idaresit <idaresitimmanuel@gmail.com>
2022-05-20 23:24:23 +0100

    initial commit

---
+++ app/src/main/java/com/example/javanoteapp/NoteListActivity.java
@@ -0,0 +1,16 @@
+package com.example.javanoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note_list_activity);
+
+
+    }
+}
